99c56b5afec8d7222e35afb0252df931eaa0a726
Kenneth <nitramnek@gmail.com>
2023-03-24 21:17:18 +0300

    Assignment Complete

---
+++ index.js
@@ -0,0 +1,15 @@
+//k3n5c0d3
+function introduction(name)
+  {
+    return`Hi, my name is ${name}.`;
+  };
+
+  function introductionWithLanguage(name, language)
+  {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+  };
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") 
+  {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
